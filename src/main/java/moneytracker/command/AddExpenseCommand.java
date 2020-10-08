4fb6e951ae95a81267607f131344c36c28ecd711
Yingtong <dgc5213@gmail.com>
2020-10-08 19:42:30 +0800

    Add List Round1.2

--- src/main/java/moneytracker/command/AddExpenseCommand.java
+++ src/main/java/moneytracker/command/AddExpenseCommand.java
@@ -21,21 +21,7 @@ public class AddExpenseCommand extends Command {
 
     @Override
     public void execute(TransactionList transactions, Ui ui, Storage storage) throws MoneyTrackerException {
-        Transaction testExpense1 = new Expense(100, "Expensive dinner",
-                "2020-12-25", "FOOD");
-        System.out.println(testExpense1);
-        Transaction testExpense2 = new Expense(200, "Expensive dinner",
-                "2020-12-25", "FOOD");
-        System.out.println(testExpense2);
-        Transaction testExpense3 = new Expense(300, "Expensive dinner",
-                "2020-12-25", "FOOD");
-        System.out.println(testExpense3);
-
-        transactions.addTransaction(testExpense1);
-        transactions.addTransaction(testExpense2);
-        transactions.addTransaction(testExpense3);
-        transactions.setIsInitialized(false);
-        System.out.println("added new things to expenses transaction");
+  
 
     }
 }
