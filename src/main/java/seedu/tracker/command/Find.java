6210d0e4b0630f758309959d467163e6ac2440f6
linqing42 <linqing42@hotmail.com>
2020-10-11 17:31:59 +0800

    add FindCommand and some error with InCharge

--- src/main/java/seedu/tracker/command/Find.java
+++ src/main/java/seedu/tracker/command/Find.java
@@ -1,4 +1,41 @@
 package seedu.tracker.command;
 
-public class Find {
+import seedu.tracker.project.ProjectList;
+import seedu.tracker.storage.Storage;
+import seedu.tracker.ui.Ui;
+
+public class Find extends Command{
+    public static final String word = "--find";
+
+    public Find(String line, ProjectList projects, Ui ui, Storage storage) {
+        super(line, projects, ui, storage);
+    }
+
+    @Override
+    public void execute() {
+        String searchWord = line.trim();
+        if (projects.size() == 0) {
+            ui.printBorderline("It appears that you have no project! Perhaps you should start creating one?");
+            return;
+        }
+        if (searchWord.isEmpty()) {
+            ui.printBorderline("Please type in the 'find <word>' format!");
+            return;
+        }
+        String matches = "Here are the project that matches '" + searchWord + "'!\n";
+        boolean hasMatch = false;
+        for (int i = 0; i < projects.size(); i++) {
+            if (projects.get(i).getDescription().toLowerCase().contains(searchWord)) {
+                matches = matches.concat((i + 1) + ". " + projects.get(i).getDescription()) + "\n";
+                hasMatch = true;
+            }
+        }
+        if (!hasMatch) {
+            matches = "It appears that are no matches for '" + searchWord + "'!\n" +
+                    "Perhaps you could try searching another word?";
+        }
+        ui.printBorderline(matches);
+
+    }
+
 }
--- src/main/java/seedu/tracker/command/InCharge.java
+++ src/main/java/seedu/tracker/command/InCharge.java
@@ -1,4 +1,32 @@
 package seedu.tracker.command;
+import seedu.tracker.exception.TrackerException;
+import seedu.tracker.project.Project;
+import seedu.tracker.project.ProjectList;
+import seedu.tracker.storage.Storage;
+import seedu.tracker.ui.Ui;
 
-public class InCharge {
+import java.io.IOException;
+
+public class InCharge extends Command {
+    public static final String word = "--inCharge";
+
+    public InCharge(String line, ProjectList projects, Ui ui, Storage storage) {
+        super(line, projects, ui, storage);
+    }
+
+    @Override
+    public void execute() {
+        try {
+            if (line.isEmpty()) {
+                throw new TrackerException("It seems that you missed out the task description!\n" +
+                        "Please type in the '--inCharge <something>' format.");
+            }
+            line = "[InCharge]     " + line;
+            projects.add(new Project(line));
+        } catch (TrackerException e) {
+            ui.printBorderline(e.getMessage());
+        }
+    }
+
 }
+
