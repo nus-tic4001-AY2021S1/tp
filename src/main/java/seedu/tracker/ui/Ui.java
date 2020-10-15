1c333f76ad1eb2735d4a0188c2bf153af377bb68
Impala36 <zhenquan38@gmail.com>
2020-10-15 22:26:59 +0800

    Shifted displaying of project to Ui so it can be used by project, delete & list

--- src/main/java/seedu/tracker/ui/Ui.java
+++ src/main/java/seedu/tracker/ui/Ui.java
@@ -1,5 +1,6 @@
 package seedu.tracker.ui;
 
+import seedu.tracker.project.Project;
 import seedu.tracker.project.ProjectList;
 
 /**
@@ -13,12 +14,14 @@ public class Ui {
     public String colorGreen(String input) {
         return "\033[33m" + input + "\033[0m";
     }
+
     /**
      * Prints the input string with a borderline.
      */
     public void printBorderline(String input) {
         System.out
-            .println(input + "\n________________________________________________________________________________________________________________________________________________________\n");
+            .println(input +
+                "\n________________________________________________________________________________________________________________________________________________________\n");
     }
 
     public void printGreeting() {
@@ -31,7 +34,8 @@ public class Ui {
     public void printHelp() {
         String help = "What you can tell me to do is listed below:\n"
             + " - See all commands            | " + colorGreen("--help\n")
-            + " - Create a new project        | " + colorGreen("--project --name INPUT --description INPUT --involve INPUT --startdate dd/mm/yyyy --duedate dd/mm/yyyy --incharge INPUT\n")
+            + " - Create a new project        | " + colorGreen(
+            "--project --name INPUT --description INPUT --involve INPUT --startdate dd/mm/yyyy --duedate dd/mm/yyyy --incharge INPUT\n")
             + " - Replace a project detail    | " + colorGreen("--replace INDEX --commandName INPUT\n")
             + " - Add an extra project detail | " + colorGreen("--add INDEX --commandName INPUT\n")
             + " - Delete a project            | " + colorGreen("--delete INDEX\n")
@@ -48,14 +52,14 @@ public class Ui {
 
     public void printProjectCreated(ProjectList projects) {
         String added = "Great! You have created a proper project as shown below:\n"
-            + colorGreen(projects.get(projects.size() - 1).getDescription()
+            + colorGreen(displayProject(projects.get(projects.size() - 1))
             + "\nWe now have " + projects.size() + " project(s) in your list!");
         printBorderline(added);
     }
 
     public void printProjectRemoved(ProjectList projects, int index) {
         String added = "Okay! We have removed the project as shown below:\n"
-            + colorRed(projects.get(index - 1).getDescription()
+            + colorRed(displayProject(projects.get(index - 1))
             + colorGreen("\nWe now have " + (projects.size() - 1) + " project(s) in your list!"));
         printBorderline(added);
     }
@@ -85,8 +89,14 @@ public class Ui {
         printBorderline("Got it. I've edited the information in the project. \n");
     }
 
-    public void printString(String s){
-        System.out.println(s);
-    }
+    public String displayProject(Project project) {
+        String[] projectDetails = project.getDescription().split("--");
 
+        return "Project Name: " + projectDetails[1].split(" ", 2)[1] + "\n"
+            + "Project Description: " + projectDetails[2].split(" ", 2)[1] + "\n"
+            + "Project Team Members: " + projectDetails[3].split(" ", 2)[1] + "\n"
+            + "Start Date: " + projectDetails[4].split(" ", 2)[1] + "\n"
+            + "Due Date: " + projectDetails[5].split(" ", 2)[1] + "\n"
+            + "Person in Charge: " + projectDetails[6].split(" ", 2)[1] + "\n";
+    }
 }
