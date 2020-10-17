1213c3f55e7f445b5437eb72b039a374bd2d4e65
linqing42 <linqing42@hotmail.com>
2020-10-17 16:02:55 +0800

    done command, email address and client, update and update related UG

--- src/main/java/seedu/tracker/ui/Ui.java
+++ src/main/java/seedu/tracker/ui/Ui.java
@@ -35,7 +35,8 @@ public class Ui {
         String help = "What you can tell me to do is listed below:\n"
                 + " - See all commands            | " + colorGreen("--help\n")
                 + " - Create a new project        | " + colorGreen(
-                "--project --name INPUT --description INPUT --involve INPUT --startdate dd/mm/yyyy --duedate dd/mm/yyyy --incharge INPUT\n")
+                "--project --name INPUT --description INPUT --involve INPUT --client INPUT --startdate dd/mm/yyyy --duedate dd/mm/yyyy --incharge INPUT --email INPUT \n")
+                + " - Complete a project          | " + colorGreen("--done INDEX\n")
                 + " - Replace a project detail    | " + colorGreen("--replace INDEX --commandName INPUT\n")
                 + " - Add an extra project detail | " + colorGreen("--add INDEX --commandName INPUT\n")
                 + " - Delete a project            | " + colorGreen("--delete INDEX\n")
@@ -95,10 +96,18 @@ public class Ui {
         return "Project Name: " + projectDetails[1].split(" ", 2)[1] + "\n"
                 + "Project Description: " + projectDetails[2].split(" ", 2)[1] + "\n"
                 + "Project Team Members: " + projectDetails[3].split(" ", 2)[1] + "\n"
-                + "Start Date: " + projectDetails[4].split(" ", 2)[1] + "\n"
-                + "Due Date: " + projectDetails[5].split(" ", 2)[1] + "\n"
-                + "Person in Charge: " + projectDetails[6].split(" ", 2)[1] + "\n"
-                + "email: " + projectDetails[7].split(" ", 2)[1] + "\n"
-                + "duration: " + projectDetails[8].split(" ", 2)[1] + "\n";
+                + "Client: " + projectDetails[4].split(" ", 2)[1] + "\n"
+                + "Start Date: " + projectDetails[5].split(" ", 2)[1] + "\n"
+                + "Due Date: " + projectDetails[6].split(" ", 2)[1] + "\n"
+                + "Person in Charge: " + projectDetails[7].split(" ", 2)[1] + "\n"
+                + "email: " + projectDetails[8].split(" ", 2)[1] + "\n"
+                + "duration: " + projectDetails[9].split(" ", 2)[1] + "\n";
+    }
+
+    public void printTaskCompleted(int index, ProjectList tasks) {
+        String done = "Nice! You have completed following project:\n"
+                + "[Completed] \033[33mProject [" + index + "] "
+                + tasks.get(index - 1).getDescription() + " set as completed!\033[0m";
+        printBorderline(done);
     }
 }
