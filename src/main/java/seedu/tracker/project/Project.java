88bab4f4b3453b95d2717d98fb4368d19ef061d3
linqing42 <linqing42@hotmail.com>
2020-10-23 21:17:16 +0800

    update

--- src/main/java/seedu/tracker/project/Project.java
+++ src/main/java/seedu/tracker/project/Project.java
@@ -2,26 +2,13 @@ package seedu.tracker.project;
 
 public class Project {
     String description;
-    protected boolean isDone;
 
     public Project(String description) {
         this.description = description;
-        this.isDone = false;
-    }
-    public String getStatusIcon() {
-        return (isDone ? "Completed" : "incomplete");
-    }
-    public Project markAsDone() {
-        this.isDone = true;
-        return this;
     }
 
     public String getDescription() {
         return description;
-    }
-
-    public String toString(){
-        return "[" + getStatusIcon() + "] " + description;
     }
 
 }
