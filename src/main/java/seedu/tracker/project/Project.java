49f6d4b16a2c0e135e1abbc6cf38cbdb549891b5
linqing42 <linqing42@hotmail.com>
2020-10-31 00:58:09 +0800

    add CompleteTest, update, projectListTestStorageTest and UiTest

--- src/main/java/seedu/tracker/project/Project.java
+++ src/main/java/seedu/tracker/project/Project.java
@@ -7,6 +7,7 @@ public class Project {
     public Project(String description) {
         this.description = description;
     }
+
     public void setComplete() {
         this.isComplete =true;
     }
--- src/test/java/seedu/tracker/UiTest.java
+++ src/test/java/seedu/tracker/UiTest.java
@@ -1,11 +1,14 @@
 package seedu.tracker;
 
 import org.junit.jupiter.api.Test;
+import seedu.tracker.common.DateConverter;
 import seedu.tracker.project.ProjectList;
 import seedu.tracker.storage.Storage;
 import seedu.tracker.ui.Ui;
 import seedu.tracker.project.Project;
 
+
+import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,21 +20,33 @@ public class UiTest {
     @Test
     public void testDisplayProject() {
 
-        Project project1=new Project("--name testing project 1 --description this is a testing project --involve Anna, Tim and Zak --startdate 10/10/2020 --duedate 12/12/2020 --incharge Alex");
-        ProjectList projects= new ProjectList();
+        Project project1 = new Project("--name Clinical Trials --description regarding hospital task --involve Tom, Lucy --client Desi --startdate 11/11/2020 --duedate 12/12/2020 --incharge Derek --email linqing4267@gmail.com --duration 31 --status Incomplete");
+        ProjectList projects = new ProjectList();
         projects.add(project1);
+        String daysLeft=null;
 
         String projectDetail1 = ui.displayProject(projects.get(0));
 
+        try {
+            daysLeft = new DateConverter("12/12/2020").getDaysLeft();
+        } catch (ParseException e) {
+            ui.printBorderline(e.getMessage());
+        }
 
         String projectDetail2 =
-                  "Project Name: testing project 1 " + "\n"
-                + "Project Description: this is a testing project " + "\n"
-                + "Project Team Members: Anna, Tim and Zak " + "\n"
-                + "Start Date: 10/10/2020 " + "\n"
-                + "Due Date: 12/12/2020 " + "\n"
-                + "Person in Charge: Alex" + "\n";
+                "Project Name: Clinical Trials " + "\n"
+                        + "Project Description: regarding hospital task " + "\n"
+                        + "Project Team Members: Tom, Lucy " + "\n"
+                        + "Client: Desi " + "\n"
+                        + "Start Date: 11/11/2020 " + "\n"
+                        + "Due Date: 12/12/2020 " + "\n"
+                        + "Person in Charge: Derek " + "\n"
+                        + "Email: linqing4267@gmail.com "  + "\n"
+                        + "Duration: 31 " + "\n"
+                        + "Days Left: "+daysLeft+ "\n"
+                        + "Status: " + project1.getStatusIcon()+ "\n";
         assertEquals(projectDetail1, projectDetail2);
+
     }
 
 }
